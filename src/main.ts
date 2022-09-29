cf84889178342f19e8962239a62b5a2f4834f3e6
AleksanderRadziszewski <radziszewski.aleksander@gmail.com>
2022-09-29 11:10:25 +0200

    auth_jwt

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,6 @@ import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(
-    session({
-      secret: `${process.env.SECRET}`,
-      resave: false,
-      saveUninitialized: false,
-      cookie: { maxAge: 3600000 },
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
   await app.listen(3000);
 }
 bootstrap();
